a985fa276aed99e84fd7e1c1de686493677f925a
WseWild <wsevs@protonmail.com>
2023-04-18 09:05:41 +0300

    add gendiff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/WseWild/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/WseWild/frontend-project-46/actions)
+
+https://asciinema.org/a/Wa2lyPJfz1dm1tL8iq4mG51qX
--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import sortBy from 'lodash.sortby';
+import * as fs from 'fs';
 
 const program = new Command();
 program
@@ -8,6 +10,41 @@ program
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const file1 = JSON.parse(fs.readFileSync(filepath1).toString());
+    const file2 = JSON.parse(fs.readFileSync(filepath2).toString());
+    const result = [];
+    const file1arr = Object.entries(file1);
+    const file2arr = Object.entries(file2);
+
+    file1arr.forEach(([key, value]) => {
+      if (Object.prototype.hasOwnProperty.call(file2, key)) {
+        if (file1[key] === file2[key]) {
+          result.push({ string: `${key}: ${value}`, ident: '  ', file: 1 });
+        } else {
+          result.push(
+            { string: `${key}: ${file1[key]}`, ident: '- ', file: 1 },
+            { string: `${key}: ${file2[key]}`, ident: '+ ', file: 2 },
+          );
+        }
+      } else {
+        result.push(
+          { string: `${key}: ${file1[key]}`, ident: '- ', file: 1 },
+        );
+      }
+    });
+
+    file2arr.forEach(([key, value]) => {
+      if (!Object.prototype.hasOwnProperty.call(file1, key)) {
+        result.push({ string: `${key}: ${value}`, ident: '+ ', file: 2 });
+      }
+    });
+
+    const sorted = sortBy(result, ['file', 'string']);
+    console.log(`{\n${sorted.map((str) => `  ${str.ident}${str.string}`).join('\n')}\n}`);
+
+    return sorted;
+  });
 
 program.parse();
